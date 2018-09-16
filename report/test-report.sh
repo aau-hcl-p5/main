ebc2ecc69f2f3cd0342bdcc6f15c38524b8cc089
Thomas Knudsen <teknight@live.dk>
2018-09-16 18:51:28 +0200

    Fix error

--- report/test-report.sh
+++ report/test-report.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(! find -mindepth 2 -name "*.tex" | xargs grep -E '[.!?]\s\b') || ((echo "ERROR! Found one or more matched of illegal regex pattern" >&2); exit 1)
+(! find -mindepth 2 -name "*.tex" | xargs grep -E '[.!?]\s\b') || ((echo "ERROR! Found one or more matches of illegal regex pattern" >&2); exit 1)
