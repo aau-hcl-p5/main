70ddd8c5acead8159cbfd5942358455343656b14
Thomas Knudsen <teknight@live.dk>
2018-09-26 21:01:27 +0200

    Added option to clean files

--- code/nxt/build.sh
+++ code/nxt/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-docker run -it -v $DIR:/home/nxtosek/nxtOSEK/build/nxt teknight/nxtosek
+docker run -it -v $DIR:/home/nxtosek/nxtOSEK/build/nxt teknight/nxtosek ${1:-all}
--- nxtOSEK/build.sh
+++ nxtOSEK/build.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd /home/nxtosek/nxtOSEK/build/nxt
 wineconsole ../../ecrobot/../toppers_osek/sg/sg ./nxt.oil -os=ECC2 -I../../ecrobot/../toppers_osek/sg/impl_oil -template=../../ecrobot/../toppers_osek/sg/lego_nxt.sgt
-make all
+make $1
 cp ../../bin/appflash.exe .
 cp ../../bin/ramboot.exe .
