b15e1d321beaaa79412451af3f23b1f7da3f2dba
Thomas Knudsen <teknight@live.dk>
2018-09-27 21:02:09 +0200

    Updated for better output and further patches to nxtOSEK ecrobot makefile

--- code/nxt/build.sh
+++ code/nxt/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-docker run -it -v $DIR:/home/nxtosek/nxtOSEK/build/nxt teknight/nxtosek:version1 ${1:-all}
+docker run -it -v $DIR:/home/nxtosek/nxtOSEK/build/nxt teknight/nxtosek ${1:-all} | sed '/^.*toppers_osek.*warning.*$\|^.*toppers_osek.*function.*$/d'
--- nxtOSEK/build.sh
+++ nxtOSEK/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /home/nxtosek/nxtOSEK/build/nxt
-wineconsole ../../ecrobot/../toppers_osek/sg/sg ./nxt.oil -os=ECC2 -I../../ecrobot/../toppers_osek/sg/impl_oil -template=../../ecrobot/../toppers_osek/sg/lego_nxt.sgt
-make $1
 cp ../../bin/appflash.exe .
 cp ../../bin/ramboot.exe .
+wineconsole ../../ecrobot/../toppers_osek/sg/sg ./nxt.oil -os=ECC2 -I../../ecrobot/../toppers_osek/sg/impl_oil -template=../../ecrobot/../toppers_osek/sg/lego_nxt.sgt &> /dev/null
+make $1
