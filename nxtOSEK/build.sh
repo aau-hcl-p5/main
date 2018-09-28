5908ba7582eb268ee75f0987d5cb0cadbaba9453
Thomas Knudsen <teknight@live.dk>
2018-09-28 09:41:32 +0200

    Added .gitignore to ensure no build files are checked in

--- nxtOSEK/build.sh
+++ nxtOSEK/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 cd /home/nxtosek/nxtOSEK/build/nxt
-cp ../../bin/appflash.exe .
-cp ../../bin/ramboot.exe .
 wineconsole ../../ecrobot/../toppers_osek/sg/sg ./nxt.oil -os=ECC2 -I../../ecrobot/../toppers_osek/sg/impl_oil -template=../../ecrobot/../toppers_osek/sg/lego_nxt.sgt &> /dev/null
 make $1
