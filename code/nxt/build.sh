95a310c0ff6b3d9372a55098830632a0708ca1da
Thomas Knudsen <teknight@live.dk>
2018-09-28 12:33:30 +0200

    Make build fail when makefile fails

--- code/nxt/build.sh
+++ code/nxt/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-docker run -it -v $DIR:/home/nxtosek/nxtOSEK/build/nxt teknight/nxtosek ${1:-all} | sed '/^.*toppers_osek.*warning.*$\|^.*toppers_osek.*function.*$/d'
+docker run -it -v $DIR:/home/nxtosek/nxtOSEK/build/nxt teknight/nxtosek ${1:-all} | sed '/^.*toppers_osek.*warning.*$\|^.*toppers_osek.*function.*$/d' ; test ${PIPESTATUS[0]} -eq 0
