f45a2c237968b3998ddc1ba4b7661d7bfdb6f4ca
Thomas Knudsen <teknight@live.dk>
2018-09-29 11:29:04 +0200

    Hopefully fixed problem regarding docker interactive mode

--- code/nxt/build.sh
+++ code/nxt/build.sh
@@ -3,4 +3,4 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-docker run -it -v $DIR:/home/nxtosek/nxtOSEK/build/nxt teknight/nxtosek ${1:-all} | sed '/^.*toppers_osek.*warning.*$\|^.*toppers_osek.*function.*$/d' ; test ${PIPESTATUS[0]} -eq 0
+docker run -v $DIR:/home/nxtosek/nxtOSEK/build/nxt teknight/nxtosek ${1:-all} 2>&1 | grep -v "toppers_osek.*warning\|toppers_osek.*function" ; test ${PIPESTATUS[0]} -eq 0
