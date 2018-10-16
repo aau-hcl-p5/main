ccfb27ed5f9651046c999b77810cbd2e0d2acc39
Thomas Knudsen <teknight@live.dk>
2018-10-16 16:11:27 +0200

    Made Thomas more happy

--- code/nxt/inc/logic.h
+++ code/nxt/inc/logic.h
@@ -2,22 +2,13 @@
 #define LOGIC_H
 #include <stdbool.h>
 
-typedef struct T_POINT {
-  int x;
-  int y;
-  unsigned int timestamp;
-} T_POINT;
+#include "target_location.h"
 
 extern void main_loop();
-
 extern bool move_to(unsigned int motor_id, int degrees_x, int degrees_y);
-
 extern bool toggle_laser(bool toggle);
-
 extern bool standby();
-
 extern int get_distance(unsigned int sensor_id);
-
-extern bool predict_trajectory(T_POINT point);
+extern bool predict_trajectory(T_TARGET_LOCATION point);
 
 #endif
--- code/nxt/src/logic.c
+++ code/nxt/src/logic.c
@@ -2,35 +2,36 @@
  * This files is mostly created in order to have a starting point for additional files besides nxt.c
  * Feel free to remove it.
  */
-#include "stdbool.h"
+#include <stdbool.h>
+#include <stdint.h>
+
 #include "nxt.h"
 
 // TODO: Not global var
 bool laser_state = false;
 
 void main_loop() {
-  ecrobot_status_monitor("HCL-P5 READY!");
-  systick_wait_ms(500); /* 500msec wait */
+    ecrobot_status_monitor("HCL-P5 READY!");
+    systick_wait_ms(500); /* 500msec wait */
 }
 
-
-bool move_to(uint motor_id, int degrees) { 
-  return true;
+bool move_to(uint32_t motor_id, int32_t degrees) {
+    return true;
 }
 
 bool toggle_laser() {
-  return (laser_state = !laser_state);
+    return (laser_state = !laser_state);
 }
 
-bool standby(bool standby) { 
-  // Do actual stuff here
-  return standby;
+bool standby(bool standby) {
+    // Do actual stuff here
+    return standby;
 }
 
-int get_distance(uint sensor_id) { 
-  return 0;
+uint32_t get_distance(uint32_t sensor_id) {
+    return 0;
 }
 
-int predict_trajectory(int pos_x, int pos_y){
-	return 0;
+int predict_trajectory(int32_t pos_x, int32_t pos_y){
+    return 0;
 }
