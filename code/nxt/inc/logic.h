ab91f18068ddaa9b1476220f67f6d2b41a19bb8e
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2018-10-16 15:08:31 +0200

    Fix: P5-71 -

--- code/nxt/inc/logic.h
+++ code/nxt/inc/logic.h
@@ -1,6 +1,23 @@
 #ifndef LOGIC_H
 #define LOGIC_H
+#include <stdbool.h>
+
+typedef struct T_POINT {
+  int x;
+  int y;
+  unsigned int timestamp;
+} T_POINT;
 
 extern void main_loop();
+
+extern bool move_to(unsigned int motor_id, int degrees_x, int degrees_y);
+
+extern bool toggle_laser(bool toggle);
+
+extern bool standby();
+
+extern int get_distance(unsigned int sensor_id);
+
+extern bool predict_trajectory(T_POINT point);
 
 #endif
