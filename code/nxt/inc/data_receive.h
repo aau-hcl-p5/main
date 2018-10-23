765736b72e261deea5d9456bb0f11c770e65acf0
Thomas Knudsen <teknight@live.dk>
2018-10-23 17:07:37 +0200

    Make tests pass

--- code/nxt/inc/data_receive.h
+++ code/nxt/inc/data_receive.h
@@ -1,6 +1,8 @@
 #ifndef USB_H
 #define USB_H
 
+#include "target_location.h"
+
 #define DISCONNECT_REQ 0xFFFFFFFF
 extern T_TARGET_LOCATION get_target_location();
 
--- code/nxt/inc/movement.h
+++ code/nxt/inc/movement.h
@@ -6,7 +6,6 @@
 
 // actual methods
 extern T_TARGET_LOCATION get_current_location();
-extern T_TARGET_LOCATION get_target_location();
 
 extern bool init_motor(uint8_t motor_id, char orientation, uint16_t speed);
 extern bool release_motor(uint8_t motor_id);
