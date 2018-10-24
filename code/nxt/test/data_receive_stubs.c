6c1a44713daee749d1e4a1ff2f258fae8187143a
Thomas Knudsen <teknight@live.dk>
2018-10-24 12:21:56 +0200

    Make tests pass

--- code/nxt/test/data_receive_stubs.c
+++ code/nxt/test/data_receive_stubs.c
@@ -1,6 +1,8 @@
 #include "data_receive.h"
+#include "stdbool.h"
 
-T_TARGET_LOCATION get_target_location() {
-    T_TARGET_LOCATION loc = {0, 0, 0};
-    return loc;
+bool get_target_location(T_TARGET_LOCATION *loc) {
+    T_TARGET_LOCATION new_loc = {0, 0, 0};
+    *loc = new_loc;
+    return true;
 }
