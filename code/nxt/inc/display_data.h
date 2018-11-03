2aeb75f71b694a5c1fd67ead17667afffb4e8769
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-11-03 12:57:46 +0100

    Fixes: P5-120 change nxt usbread init screen (#80)
    * Added new display module.
    
    * Updated target pos text.
    
    * Updated a comment
    
    * Decreased yelling, moved comp.time. to init
    
    * Added display methods.
    
    * Commas are a part of healthy code.
    
    * Changed parameters to int32_t

--- code/nxt/inc/display_data.h
+++ code/nxt/inc/display_data.h
@@ -3,6 +3,8 @@
 
 #include "target_location.h"
 
+extern void display_string_at_xy(uint8_t x, uint8_t y, char *str);
+extern void display_int_at_xy(uint8_t x, uint8_t y, int32_t number, int32_t spaces);
 extern void display_target_location(T_TARGET_LOCATION loc);
 
 #endif
