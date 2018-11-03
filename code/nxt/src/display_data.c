3eec3b34f5f42297b67c75af41ffe7aa401579ef
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-11-03 12:30:29 +0100

    Decreased yelling, moved comp.time. to init

--- code/nxt/src/display_data.c
+++ code/nxt/src/display_data.c
@@ -9,14 +9,8 @@ void display_target_location(T_TARGET_LOCATION loc)
 {
     display_clear(0);
     display_update();
-   
-    /* SHOW COMPILATION TIME TO CHECK IF RIGHT VERSION IS RUNNING */
-    display_goto_xy(0, 0);
-    display_string("C.TIME:");
-    display_goto_xy(8, 0);
-    display_string(__TIME__);
 
-    /* IS TARGET FOUND? */
+    /* Is target found? */
     T_TARGET_LOCATION target_location;
     get_target_location(&target_location);     
     display_goto_xy(0, 1);
@@ -29,7 +23,7 @@ void display_target_location(T_TARGET_LOCATION loc)
     }
 
 
-    /* POSITION */
+    /* Position */
     T_TARGET_LOCATION current_location = get_current_location();
     display_goto_xy(0, 2);
     display_string("POSITION:");
@@ -44,7 +38,7 @@ void display_target_location(T_TARGET_LOCATION loc)
     display_goto_xy(12, 3);
     display_int(current_location.y, 4);
 
-    /* POWER */
+    /* Power */
     display_goto_xy(0, 4);
     display_string("POWER:");
 
@@ -58,7 +52,7 @@ void display_target_location(T_TARGET_LOCATION loc)
     display_goto_xy(12, 5);
     display_int(get_speed_by_distance(loc.y,'y'), 4);
 
-    /* MODIFIER */
+    /* Modifier */
     display_goto_xy(0, 6);
     display_string("MODIFIER:");
 
--- code/nxt/src/init_screen.c
+++ code/nxt/src/init_screen.c
@@ -8,5 +8,9 @@ void show_init_screen(void)
 	display_string("F.L.A.T.");
 	display_goto_xy(0, 1);
 	display_string("Awaiting USB host.");
+	display_goto_xy(0, 3);
+    display_string("Compilation time:");
+    display_goto_xy(0, 4);
+    display_string(__TIME__);
 	display_update();	
 }
