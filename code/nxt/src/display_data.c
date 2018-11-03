79cf3043161a90a92711678a979f661451d98712
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-11-03 12:44:58 +0100

    Added display methods.

--- code/nxt/src/display_data.c
+++ code/nxt/src/display_data.c
@@ -5,6 +5,16 @@
 #include "data_receive.h"
 #include "movement.h"
 
+void display_string_at_xy(uint8_t x, uint8_t y, char *str){
+    display_goto_xy(x, y);
+    display_string(str);
+}
+
+void display_int_at_xy(uint8_t x, uint8_t y, int number, int spaces){
+    display_goto_xy(x, y);
+    display_int(number, spaces);
+}
+
 void display_target_location(T_TARGET_LOCATION loc)
 {
     display_clear(0);
@@ -13,58 +23,36 @@ void display_target_location(T_TARGET_LOCATION loc)
     /* Is target found? */
     T_TARGET_LOCATION target_location;
     get_target_location(&target_location);     
-    display_goto_xy(0, 1);
-    display_string("TARGET");
-    display_goto_xy(9, 1);
+    display_string_at_xy(0, 1 "Target");
     if(target_location.timestamp == 0x00FF) {
-        display_string("GONE");
+        display_string_at_xy(9, 1 "gone");
     } else {
-        display_string("FOUND");
+        display_string_at_xy(9, 1 "found");
     }
 
 
     /* Position */
     T_TARGET_LOCATION current_location = get_current_location();
-    display_goto_xy(0, 2);
-    display_string("POSITION:");
-
-    display_goto_xy(0, 3);
-    display_string("X:");
-    display_goto_xy(3, 3);
-    display_int(current_location.x, 4);
-
-    display_goto_xy(9, 3);
-    display_string("Y:");
-    display_goto_xy(12, 3);
-    display_int(current_location.y, 4);
+    display_string_at_xy(0, 2, "Position");
+    display_string_at_xy(0, 3, "X:");
+    display_int_at_xy(3, 3, current_location, 4);
+    display_string_at_xy(9, 3, "Y:");
+    display_int_at_xy(12, 3, current_location.y, 4);
 
     /* Power */
-    display_goto_xy(0, 4);
-    display_string("POWER:");
-
-    display_goto_xy(0, 5);
-    display_string("X:");
-    display_goto_xy(3, 5);
-    display_int(get_speed_by_distance(loc.x,'x'), 4);
-
-    display_goto_xy(9, 5);
-    display_string("Y:");
-    display_goto_xy(12, 5);
-    display_int(get_speed_by_distance(loc.y,'y'), 4);
+    display_string_at_xy(0, 4, "Power:");
+    display_string_at_xy(0, 5, "X:");
+    display_int_at_xy(3, 5, get_speed_by_distance(loc.x, 'x'), 4);
+    display_string_at_xy(9, 5, "Y:");
+    display_int_at_xy(12, 5, get_speed_by_distance(loc.y, 'y'), 4);
 
     /* Modifier */
-    display_goto_xy(0, 6);
-    display_string("MODIFIER:");
+    display_string_at_xy(0, 6, "Modifier:");
+    display_string_at_xy(0, 7, "X:");
+    display_int_at_xy(3, 7, x_lower_bound_modifier, 4);
 
-    display_goto_xy(0, 7);
-    display_string("X:");
-    display_goto_xy(3, 7);
-    display_int(x_lower_bound_modifier, 4);
-
-    display_goto_xy(9, 7);
-    display_string("Y:");
-    display_goto_xy(12, 7);
-    display_int(y_lower_bound_modifier, 4);
+    display_string_at_xy(9, 7, "Y:");
+    display_int_at_xy(12, 7, y_lower_bound_modifier, 4);
 
     display_update();
 }
--- code/nxt/src/init_screen.c
+++ code/nxt/src/init_screen.c
@@ -1,16 +1,13 @@
 #include "nxt.h"
 #include "init_screen.h"
+#include "display_data.h"
 
 void show_init_screen(void)
 {
 	display_clear(0);
-	display_goto_xy(0, 0);
-	display_string("F.L.A.T.");
-	display_goto_xy(0, 1);
-	display_string("Awaiting USB host.");
-	display_goto_xy(0, 3);
-    display_string("Compilation time:");
-    display_goto_xy(0, 4);
-    display_string(__TIME__);
+	display_string_at_xy(0, 0, "F.L.A.T");
+	display_string_at_xy(0, 1, "Awaiting USB Host.");
+	display_string_at_xy(0, 3, "Compilation time:");
+	display_string_at_xy(0, 4, __TIME__);
 	display_update();	
 }
