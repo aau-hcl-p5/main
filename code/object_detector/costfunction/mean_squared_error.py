ed33fa07fa44d5d4f2d4787e20f1cb4937baa471
Nlisby <nillas12@gmail.com>
2018-11-06 13:05:20 +0100

    More error proof code

--- code/object_detector/costfunction/mean_squared_error.py
+++ code/object_detector/costfunction/mean_squared_error.py
@@ -15,17 +15,20 @@ class Mean_Squared_Error(GenericCostFunction):
     :return: a value between 0 and 1 indicating the cost, deviation from the expected.
     """
     def compute_cost(data: np.ndarray[Vector], predictions: np.ndarray[Vector]) -> NumberType:
-        samples_num = len(predictions)
+        
+        datalength = len(data)
+        predictionslength = len(predictions)
+
+        if datalength != predictionslength:
+            raise Exception('Prediction set and Data set must be of same length')
+        if predictionslength == 0:
+            raise Exception('Length of predictions must not be zero')
     
         accumulated_error = 0.0
         for prediction, target in zip(predictions, data):
             accumulated_error += (prediction - target).length()**2
 
         # Calculating mean and dividing by 2
-        cost = (1.0 / (2 * samples_num)) * accumulated_error
+        cost = (1.0 / (2 * predictionslength)) * accumulated_error
     
         return cost
-
-
-
-
