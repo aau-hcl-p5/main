af5f504eb734a68aba6b4aa28d16b55891b0da72
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-11-22 20:32:26 +0100

    Removed timers

--- code/nxt/src/nxt.c
+++ code/nxt/src/nxt.c
@@ -25,16 +25,6 @@ DeclareResource(USB_Rx);
 /* Global variables */
 T_TARGET_INFORMATION target_information = { 0, 0, DISCONNECT_REQ };
 
-int UpdateDisplayStart = 0, UpdateDisplayEnd = 0, 
-    ToggleLaserStart = 0,   ToggleLaserEnd = 0,
-    ReceiveDataStart = 0,   ReceiveDataEnd = 0, 
-    MoveMotorsStart = 0,    MoveMotorsEnd = 0;
-
-int UpdateDisplayWCET = 0,  UpdateDisplayBCET = 0,
-    ToggleLaserWCET = 0,    ToggleLaserBCET = 0,
-    ReceiveDataWCET = 0,    ReceiveDataBCET = 0,
-    MoveMotorsWCET = 0,     MoveMotorsBCET = 0;
-
 
 /* Initializes motors with their direction */
 void ecrobot_device_initialize(void)
@@ -50,51 +40,29 @@ void ecrobot_device_terminate(void)
   release_motor(NXT_PORT_B);
   ecrobot_term_usb(); /* terminate USB */
 }
-int count = 0;
+
 /* nxtOSEK hook to be invoked from an ISR in category 2 */
 void user_1ms_isr_type2(void)
 {
   /* Increment System Timer Count to activate periodical Tasks */
   (void)SignalCounter(SysTimerCnt);
-  count++;
 }
 
 TASK(UpdateDisplay)
 {
-  UpdateDisplayStart = count;
-
   if(target_information.status == DISCONNECT_REQ)
     {
       show_init_screen();
     }
     else
     {
-      /* Timing methods */
-      display_clear(0);
-      display_int_at_xy(0, 0, UpdateDisplayWCET, 6);
-      display_int_at_xy(0, 1, ToggleLaserWCET, 6);
-      display_int_at_xy(0, 2, ReceiveDataWCET, 6);
-      display_int_at_xy(0, 3, MoveMotorsWCET, 6);
-      display_int_at_xy(0, 3, count, 6);
-
-      display_update();
-      
-      // display_target_information(target_information);
+      display_target_information(target_information);
     }
- 
-  UpdateDisplayEnd = count;
-
-  if(UpdateDisplayWCET < (UpdateDisplayEnd-UpdateDisplayStart))
-    UpdateDisplayWCET = (UpdateDisplayEnd-UpdateDisplayStart);
-  if(UpdateDisplayBCET > (UpdateDisplayEnd-UpdateDisplayStart))
-    UpdateDisplayBCET = (UpdateDisplayEnd-UpdateDisplayStart);
-
   TerminateTask();
 }
 
 TASK(ToggleLaser)
 {
-  ToggleLaserStart = count;
   while(1){
     WaitEvent(LaserOnEvent);
     ClearEvent(LaserOnEvent);
@@ -102,19 +70,11 @@ TASK(ToggleLaser)
     WaitEvent(LaserOffEvent);
     ClearEvent(LaserOffEvent);
   }
-  ToggleLaserEnd = count;
-
-  if(ToggleLaserWCET < (ToggleLaserEnd-ToggleLaserStart))
-    ToggleLaserWCET = (ToggleLaserEnd-ToggleLaserStart);
-  if(ToggleLaserBCET > (ToggleLaserEnd-ToggleLaserStart))
-    ToggleLaserBCET = (ToggleLaserEnd-ToggleLaserStart);
-
   TerminateTask();
 }
 
 TASK(ReceiveData)
 {
-  ReceiveDataStart = count;
 	GetResource(USB_Rx);
   ecrobot_process1ms_usb();
   ReleaseResource(USB_Rx);
@@ -127,19 +87,11 @@ TASK(ReceiveData)
       SetEvent(MoveMotors, MoveMotorsOffEvent);
     }
   }
-  ReceiveDataEnd = count;
-
-  if(ReceiveDataWCET < (ReceiveDataEnd-ReceiveDataStart))
-    ReceiveDataWCET = (ReceiveDataEnd-ReceiveDataStart);
-  if(ReceiveDataBCET > (ReceiveDataEnd-ReceiveDataStart))
-    ReceiveDataBCET = (ReceiveDataEnd-ReceiveDataStart);
-  
   TerminateTask();
 }
 
 TASK(MoveMotors)
 {
-  MoveMotorsStart = count;
   while(1){
     WaitEvent(MoveMotorsOnEvent);
     ClearEvent(MoveMotorsOnEvent);
@@ -150,12 +102,5 @@ TASK(MoveMotors)
     ClearEvent(MoveMotorsOffEvent);
     stop_motors();
   }
-  MoveMotorsEnd = count;
-
-  if(MoveMotorsWCET < (MoveMotorsEnd-MoveMotorsStart))
-    MoveMotorsWCET = (MoveMotorsEnd-MoveMotorsStart);
-  if(MoveMotorsBCET > (MoveMotorsEnd-MoveMotorsStart))
-    MoveMotorsBCET = (MoveMotorsEnd-MoveMotorsStart);
-
   TerminateTask();
 }
