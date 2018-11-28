504d9ba109fcd7762f3f5b110c7179051289ec46
Casper Bang <a@thecdk.net>
2018-11-28 12:39:41 +0100

    fixed lint?

--- code/object_detector/test/test_algorithms.py
+++ code/object_detector/test/test_algorithms.py
@@ -5,40 +5,8 @@ but rather tests subsets of the process in smaller simplistic cases
 """
 import unittest
 
-import numpy as np
-
-from algorithms import Goturn, ZoneAvgController, ObjectFillController, Vector
+from algorithms import ZoneAvgController, ObjectFillController, Vector
 from webcam import VideoController, CaptureDeviceType
-
-
-class TestGoturnAlgorithms(unittest.TestCase):
-    """
-    Test everything regarding the implementation of the GOTURN algorithm
-    This doesn't do a whole learning run, which takes time,
-    but rather tests subsets of the process in smaller simplistic cases
-    """
-
-    def setUp(self):
-        self.test_string = "fisk"
-
-    def test_instantiate_clean_goturn(self):
-        """
-        Test whether it is possible to instantiate the Goturn controller
-        """
-        self.assertEqual(Goturn().export_model(), "")
-
-    def test_predict_with_goturn(self):
-        """
-        Test we can predict where the target is,
-        based on supplied test data and a pre created model
-
-        :return:
-        """
-        self.assertEqual(
-            Vector(1, 5),
-            Goturn().predict(self.test_string),
-            msg="The returned coordinates did not match the expected output"
-        )
 
 
 class TestZoneAvgAlgorithm(unittest.TestCase):
