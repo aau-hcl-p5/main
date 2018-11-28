bcc93fe6cfc1c56eebeb5e027a4b287b06dbd3a9
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-11-28 12:36:22 +0100

    Update code/object_detector/communication/sound_debugger.py
    Co-Authored-By: C0DK <master@thecdk.net>

--- code/object_detector/communication/sound_debugger.py
+++ code/object_detector/communication/sound_debugger.py
@@ -15,7 +15,7 @@ class SoundDebugger(OutputDevice):
         if status is Status.TARGET_FOUND:
             print("Target found sound!")
         elif status is Status.NO_TARGET_FOUND:
-            print("Target disappeared :/")
+            print("Target missing")
         elif status is Status.DISCONNECT_REQ:
             print("bzzt disconnect")
         else:
