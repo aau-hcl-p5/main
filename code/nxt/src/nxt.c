9c0f00c7a02198a7918120850be84397dde7ec41
Thomas Knudsen <teknight@live.dk>
2018-11-29 11:55:34 +0100

    Inital cyclic

--- code/nxt/src/nxt.c
+++ code/nxt/src/nxt.c
@@ -5,57 +5,98 @@
 #include "usb.h"
 #include "display_manager.h"
 
-
 /* OSEK declarations */
-DeclareTask(RunCalibration);
-DeclareTask(ReceiveData);
-DeclareTask(UpdateDisplay);
-DeclareTask(ToggleLaser);
-DeclareTask(MoveMotors);
+DeclareTask(Main);
 DeclareTask(KeepUSBAlive);
 
 DeclareCounter(SysTimerCnt);
 
-DeclareEvent(MoveMotorsOnEvent);
-DeclareEvent(MoveMotorsOffEvent);
-DeclareEvent(LaserOnEvent);
-DeclareEvent(LaserOffEvent);
+DeclareEvent(CalibrationStartEvent);
+DeclareEvent(CalibrationDoneEvent);
 
 DeclareResource(USB_Rx);
 
 /* Global variables */
-T_VECTOR last_target_location = { 0, 0 };
+T_VECTOR last_target_location = {0, 0};
 STATUS_CODE current_status = DISCONNECTED_REQ;
-
+bool newMajorCycle = false;
 
 /* Initializes motors with their direction */
-void ecrobot_device_initialize(void) {
+void ecrobot_device_initialize(void)
+{
     init_motor(NXT_PORT_A, 'y', 20);
     init_motor(NXT_PORT_B, 'x', 20);
     ecrobot_init_usb(); /* init USB */
 }
 
-void ecrobot_device_terminate(void) {
+void ecrobot_device_terminate(void)
+{
     release_motor(NXT_PORT_A);
     release_motor(NXT_PORT_B);
     ecrobot_term_usb(); /* terminate USB */
 }
 
 /* nxtOSEK hook to be invoked from an ISR in category 2 */
-void user_1ms_isr_type2(void) {
+void user_1ms_isr_type2(void)
+{
     /* Increment System Timer Count to activate periodical Tasks */
     (void)SignalCounter(SysTimerCnt);
+    newMajorCycle = true;
 }
 
-TASK(RunCalibration) {
-    if(!calibrated) {
-        calibrate(false);
+/* Keep USB alive task */
+TASK(KeepUSBAlive)
+{
+    while(1){
+        WaitEvent(CalibrationStartEvent);
+        KeepUSBAlive();
+        WaitEvent(CalibrationDoneEvent);
     }
     TerminateTask();
 }
 
-TASK(UpdateDisplay) {
-    if(current_status == DISCONNECTED_REQ)
+TASK(Main)
+{
+    if (current_status == READY_FOR_CALIBRATION)
+    {
+        SetEvent(KeepUSBAlive, CalibrationStartEvent);
+        RunCalibration();
+        SetEvent(KeepUSBAlive, CalibrationDoneEvent);
+    }
+
+    while (1)
+    {
+        // Check if 1 ms has passed and a new cycle should begin
+        if (newMajorCycle)
+        {
+            KeepUSBAlive();
+            ReceiveData();
+            MoveMotors();
+            ToggleLaser();
+            newMajorCycle = false;
+        }
+    }
+    TerminateTask();
+}
+
+void KeepUSBAlive()
+{
+    GetResource(USB_Rx);
+    ecrobot_process1ms_usb();
+    ReleaseResource(USB_Rx);
+}
+
+void RunCalibration()
+{
+    if (!calibrated)
+    {
+        calibrate(false);
+    }
+}
+
+void UpdateDisplay()
+{
+    if (current_status == DISCONNECTED_REQ)
     {
         show_init_screen();
     }
@@ -63,66 +104,35 @@ TASK(UpdateDisplay) {
     {
         display_target_information(current_status, last_target_location);
     }
-    TerminateTask();
 }
 
-TASK(ToggleLaser) {
-    while(1) {
-        WaitEvent(LaserOnEvent);
-        ClearEvent(LaserOnEvent);
-
-        WaitEvent(LaserOffEvent);
-        ClearEvent(LaserOffEvent);
-    }
-    TerminateTask();
+void ToggleLaser()
+{
+    /* Toggle laser */
 }
 
-TASK(KeepUSBAlive) {
-    GetResource(USB_Rx);
-    ecrobot_process1ms_usb();
-    ReleaseResource(USB_Rx);
-    TerminateTask();
-}
-
-TASK(ReceiveData) {
-
-    if (get_status_code(&current_status)) {
-        if(current_status == TARGET_FOUND)
+void ReceiveData()
+{
+    if (get_status_code(&current_status))
+    {
+        if (current_status == TARGET_FOUND)
         {
             /* Wait for a target location */
-            while(!get_target_location(&last_target_location)) {}
-
-            SetEvent(MoveMotors, MoveMotorsOnEvent);
-        }
-        else if(current_status == NO_TARGET_FOUND) {
-            SetEvent(MoveMotors, MoveMotorsOffEvent);
-        }
-        else if(current_status == READY_FOR_CALIBRATION) {
-
-            display_clear(0);
-            for(int i = 0; i < POINTS_ON_AXIS; i++){
-                systick_wait_ms(50);
-                display_calibration_transfer_status(i, y_axis_powers[i]);
-                display_update();
-                send_calibration_data(i, true, y_axis_powers[i]);
-            }
+            /* TODO: This should not be blocking */
+            while (!get_target_location(&last_target_location)){}
         }
     }
-    TerminateTask();
 }
 
-TASK(MoveMotors) {
-    // This task is going to be preempted so this is not an issue
-    while(1) {
-
-        // we wait for the motor event, from ReceiveData, to be sent.
-        WaitEvent(MoveMotorsOnEvent);
-        ClearEvent(MoveMotorsOnEvent);
-        move(last_target_location);
-
-        WaitEvent(MoveMotorsOffEvent);
-        ClearEvent(MoveMotorsOffEvent);
+void MoveMotors()
+{
+    if (current_status == TARGET_FOUND)
+    {
         stop_motors();
     }
-    TerminateTask();
+
+    else if (current_status == NO_TARGET_FOUND)
+    {
+        move(last_target_location);
+    }
 }
