68645cf325a17a2739c17645cc99cca84da489c2
Frederik Spang <frederik@progras.dk>
2018-12-14 11:57:36 +0100

    Add more documentation

--- code/nxt/src/movement.c
+++ code/nxt/src/movement.c
@@ -19,15 +19,13 @@ uint8_t y_lower_bound_modifier = 0;
 T_VECTOR last_location = {0, 0};
 
 
-/*--------------------------------------------------------------------------*/
-/* move_motors:                                                             */
-/* ------------------------------------------------------------------------ */
-/* Description:                                                             */
-/* Params  : T_VECTOR target: Information about the target                  */
-/*              location.                                                   */
-/* Returns : None                                                           */
-/*--------------------------------------------------------------------------*/
-
+/*
+ * Function: move_motors
+ * ----------------------------
+ *   Sets motor speed based on T_VECTOR target, using get_speed_by_distance().
+ *
+ *   target: Information about the target location.
+ */
 void move(T_VECTOR target) {
     // speed is 0 when distance is small enough.
     x_motor_speed = get_speed_by_distance(target.x, AXIS_X);
@@ -38,7 +36,7 @@ void move(T_VECTOR target) {
 
 void set_motor_speed(T_AXIS_TYPE axis, int8_t speed) {
     uint8_t motor = y_motor;
-    if (axis == AXIS_X){
+    if (axis == AXIS_X) {
         motor = x_motor;
     }
 
@@ -46,17 +44,16 @@ void set_motor_speed(T_AXIS_TYPE axis, int8_t speed) {
 }
 
 
-/*--------------------------------------------------------------------------*/
-/* init_motor:                                                              */
-/* ------------------------------------------------------------------------ */
-/* Description: Used to initialize a motor before it is used the first time.*/
-/* Params  : motor_id: The ID of the motor. Value should be NXT_PORT_(port).*/
-/*           axis: Specifies if the motor handles horizontal (x) or         */
-/*                 vertical (y) movement.                                   */
-/*           speed: The speed of the motor. Value between 0 and 100.        */
-/* Returns : None                                                           */
-/*--------------------------------------------------------------------------*/
-
+/*
+ * Function: init_motor
+ * ----------------------------
+ *   Used to initialize a motor before it is used the first time.
+ *
+ *   motor_id: The ID of the motor. Value should be NXT_PORT_(port).
+ *   axis: Specifies if the motor handles horizontal (x) or
+ *         vertical (y) movement.
+ *   speed: The speed of the motor. Value between 0 and 100.
+ */
 bool init_motor(uint8_t motor_id, T_AXIS_TYPE axis, uint16_t speed) {
     if (axis == AXIS_X) {
         x_motor = motor_id;
@@ -65,7 +62,7 @@ bool init_motor(uint8_t motor_id, T_AXIS_TYPE axis, uint16_t speed) {
         nxt_motor_set_count(x_motor, 0);
         last_location.x = ecrobot_get_motor_rev(motor_id);
         return true;
-    } else if(axis == AXIS_Y) {
+    } else if (axis == AXIS_Y) {
         y_motor = motor_id;
         y_motor_speed = speed;
         ecrobot_set_motor_speed(y_motor, 0);
@@ -76,27 +73,25 @@ bool init_motor(uint8_t motor_id, T_AXIS_TYPE axis, uint16_t speed) {
     return false;
 }
 
-/*--------------------------------------------------------------------------*/
-/* stop_motors:                                                             */
-/* ------------------------------------------------------------------------ */
-/* Description: Is used to stop all motion on the motors.                   */
-/* Params  : None                                                           */
-/* Returns : None                                                           */
-/*--------------------------------------------------------------------------*/
+/* Function: stop_motors
+ * ----------------------------
+ *   Is used to stop all motion on the motors.
+ */
 
 void stop_motors() {
     nxt_motor_set_speed(x_motor, 0, 1);
     nxt_motor_set_speed(y_motor, 0, 1);
 }
 
-/*--------------------------------------------------------------------------*/
-/* release_motors:                                                          */
-/* ------------------------------------------------------------------------ */
-/* Description: Used to release the motors when they are no longer in use.  */
-/* Params  : motor_id specifies witch motor is released.                    */
-/* Returns : returns true if the release was successful                     */
-/*--------------------------------------------------------------------------*/
-// Should probably check whether motor_id is in use. But it releases the motor.
+/*
+ * Function: release_motors
+ * ----------------------------
+ *   Used to release the motors when they are no longer in use.
+ *
+ *   motor_id specifies witch motor is released.
+ *
+ *   Returns: returns true if the release was successful
+*/
 bool release_motor(uint8_t motor_id) {
     nxt_motor_set_speed(motor_id, 0, 1);
     return true;
@@ -104,11 +99,14 @@ bool release_motor(uint8_t motor_id) {
 
 
 /*
- * Description: Gets the motor speed needed, based on distance on axis
- * Params  :
- *      distance:   range: -127 -> 127 on axis
- *      axis:       'x' or 'y'
- * Returns : the motor speed (-100->100) that the motor should move
+ * Function: get_speed_by_distance
+ * ----------------------------
+ *   Gets the motor speed needed, based on distance on axis
+ *
+ *   distance: range: -127 -> 127 on axis
+ *   axis: 'x' or 'y'
+ *
+ *   Returns: the motor speed (-100->100) that the motor should move
  */
 int8_t get_speed_by_distance(int8_t distance, T_AXIS_TYPE axis) {
     if (distance < MOTOR_DEADZONE && distance > -MOTOR_DEADZONE) {
@@ -122,7 +120,13 @@ int8_t get_speed_by_distance(int8_t distance, T_AXIS_TYPE axis) {
     return (distance / 5) + lower_bound - (lower_bound / 5);
 }
 
-
+/*
+ * Function: get_current_revolution
+ * ----------------------------
+ *   Gets the current motors rev - Angle.
+ *
+ *   Returns: T_REVOLUTION, {x, y} with current rev of x and y motors.
+ */
 T_REVOLUTION get_current_revolution() {
     T_REVOLUTION revolution = {ecrobot_get_motor_rev(x_motor), ecrobot_get_motor_rev(y_motor)};
     return revolution;
--- code/nxt/test/display_stubs.c
+++ code/nxt/test/display_stubs.c
@@ -1,20 +1,8 @@
 #include "stubs.h"
 
-void display_goto_xy(uint8_t x, uint8_t y) {
-
-}
-void display_string(char *str) {
-
-}
-void display_int(int num, int len) {
-
-}
-void display_hex(int num, int len) {
-
-}
-void display_update() {
-
-}
-void display_clear(uint32_t update_too) {
-
-}
+void display_goto_xy(uint8_t x, uint8_t y) { }
+void display_string(char *str) { }
+void display_int(int num, int len) { }
+void display_hex(int num, int len) { }
+void display_update() { }
+void display_clear(uint32_t update_too) { }
